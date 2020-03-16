db4609dab199dd3600932ccdb3ef5c10bae489f5
Manujaya Premathilaka <manujayapremathilaka@gmail.com>
2020-03-16 16:14:24 +0530

    Designed main page with sample categories

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,15 +1,64 @@
 import React, {Component} from "react";
 import MainPageNavBar from "./MainPageNavBar";
 import MainPageBody from "./MainPageBody";
+import { Container, Row, Col } from 'reactstrap';
 
 class MainPage extends Component{
+
+    constructor() {
+        super();
+
+        this.state = {
+            category: [
+                {
+                    id: 1,
+                    name: "David Davidson",
+                    company: "Some Company, Inc",
+                    description: "Met at a party. Will connect with later"
+                },
+                {
+                    id: 2,
+                    name: "Mark Markson",
+                    company: "Some Company, Inc",
+                    description: "Met at a party. Will connect with later"
+                },
+                {
+                    id: 3,
+                    name: "Judy Judyson",
+                    company: "Some Company, Inc",
+                    description: "Met at a party. Will connect with later"
+                },
+                {
+                    id: 4,
+                    name: "James Jameson",
+                    company: "Some Company, Inc",
+                    description: "Met at a party. Will connect with later"
+                }
+            ]
+        }
+    }
+
     render() {
+
+        let categories = this.state.category.map(category => {
+            return(
+                <MainPageBody key ={category.id} cats = {category}/>
+            )
+        })
+
         return (
             <div>
                 <MainPageNavBar/>
-                <MainPageBody/>
+
+                <Container fluid className="d-flex justify-content-center">
+                    <Row>
+                        {categories}
+                    </Row>
+                </Container>
+
             </div>
         );
+
     }
 }
 
--- src/MainPage/MainPageBody.js
+++ src/MainPage/MainPageBody.js
@@ -1,29 +1,31 @@
 import React, {Component} from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
 
 class MainPageBody extends Component{
+
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+
+        let { id, name, company, description } = this.props.cats;
+
         return (
-            <div className="row">
-                <div className="col-sm-6">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional
-                                content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-6">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional
-                                content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
+
+            <div className="px-md-4 mt-5">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>
+                            {description}
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
             </div>
         );
     }
